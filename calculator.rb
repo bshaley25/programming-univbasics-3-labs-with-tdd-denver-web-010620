086f7074b5cc825a81e1ddf57e2ac8e4f1646bd7
Bradley Haley <bshaley25@gmail.com>
2019-11-30 18:22:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,11 @@
 first_number = 10.0  
 second_number = 5.0  
 sum = first_number + second_number
+<<<<<<< HEAD
 difference = first_number - second_number
+=======
+diffrenence = first_number - second_number
+>>>>>>> c6e4dd37d839deb2e15fa4bcc83c13211afb558a
 product = first_number * second_number
 quotient = first_number / second_number
 
